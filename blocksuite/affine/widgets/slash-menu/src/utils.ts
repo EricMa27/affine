0fef1e4910e1535d0b19ace370fb217f3ede32ab
EricMa27 <134251865+EricMa27@users.noreply.github.com>
2026-01-14 21:06:55 +0800

    Update utils.ts

--- blocksuite/affine/widgets/slash-menu/src/utils.ts
+++ blocksuite/affine/widgets/slash-menu/src/utils.ts
@@ -100,3 +100,12 @@ export function formatTime(date: Date) {
   const strTime = `${formatDate(date)} ${hours}:${minutes}`;
   return strTime;
 }
+
+export function formatTimeShort(date: Date) {
+  const year = date.getFullYear().toString().slice(2);
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  return `${year}${month}${day}-${hours}${minutes}`;
+}
